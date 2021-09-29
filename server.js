0d272db32e021a80173be9e9d6a6616f2720b52d
Jake Kaplan <kaplanjt@gmail.com>
2021-09-28 20:50:16 -0700

    This has the working starter page, having a lot of trouble fixing an issue with the index starting at 0 and not 1. Big roadblock for me at this moment.

--- server.js
+++ server.js
@@ -19,12 +19,27 @@ app.get('/pokemon/new/', (req, res) => {
     res.render("new.ejs", {title: "Add New Pokemon"});
 })
 
+//DELETE
+app.delete("/pokemon/:id", (req, res) => {
+    pokemon.splice(req.params.id, 1) 
+    res.redirect('/pokemon')
+})
+
 
 //CREATE
 app.post("/pokemon", (req, res) => {
     pokemon.push(req.body);
     res.redirect("/pokemon");
 
+})
+
+//EDIT
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.render("edit.ejs", {
+        pokemon: pokemon[req.params.id],
+        index: req.params.id,
+        title: "Pokemon Edit Page"
+    })
 })
 
 
